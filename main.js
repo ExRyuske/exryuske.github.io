1eb13c4ebdb952107ca54e5ac582a9585e3e4115
Ryuunosuke Example <120612156+ExRyuske@users.noreply.github.com>
2023-08-23 21:25:09 +0300

    UPD

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-var detailsList = document.querySelectorAll("details:not(#foo)");
-  
-  detailsList.forEach(function(details) {
-    var summary = details.querySelector("summary");
-    details.addEventListener("toggle", function() {
-      if (details.open) {
-        summary.textContent = "Show less...";
-      } else {
-        summary.textContent = "Show more...";
-      }
-    });
-  });
+function show_about_me() {
+    document.getElementById('About_me').style.display ='block';
+    document.getElementById('Tools').style.display ='none';
+    document.getElementById('Fix_issues').style.display ='none';
+}
+
+function show_tools() {
+    document.getElementById('About_me').style.display ='none';
+    document.getElementById('Tools').style.display ='block';
+    document.getElementById('Fix_issues').style.display ='none';
+}
+
+function show_fix_issues() {
+    document.getElementById('About_me').style.display ='none';
+    document.getElementById('Tools').style.display ='none';
+    document.getElementById('Fix_issues').style.display ='block';
+}
+
+function calculate() {
+    var size = document.getElementById("Size").value;
+    var speed = document.getElementById("Speed").value * 8;
+
+    var result = document.getElementById("Result");
+
+    var seconds = (8192 / (speed * 1024) * (size * 1024) * 1.05)
+    var hours = (Math.round(seconds / 3600) + " Hours");
+    var minutes = (Math.round(seconds / 60 % 60) + " Minutes");
+
+    result.textContent = "≈ " + hours + " " + minutes;
+}
